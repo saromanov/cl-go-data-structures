946db901c58baa9331fa40fc49ef5f277d185b82
Sergey Romanov <xxsmotur@gmail.com>
2015-09-02 04:04:02 +0500

    Add comments

--- concurrent/list.go
+++ concurrent/list.go
@@ -10,6 +10,8 @@ type List struct {
 	mutex sync.RWMutex
 }
 
+
+//NewList provides creates list object
 func NewList() *List {
 	l := new(List)
 	l.l = list.New()
@@ -17,30 +19,38 @@ func NewList() *List {
 	return l
 }
 
+
+//PushBack provides append new element to back
 func (l *List) PushBack(v interface{}) *list.Element {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
 	return l.l.PushBack(v)
 }
 
+//PushBack provides append new element to front
 func (l *List) PushFront(v interface{}) *list.Element {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
 	return l.l.PushFront(v)
 }
 
+//Len returns length of this list
 func (l *List) Len() int {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
 	return l.l.Len()
 }
 
-func (l *List) Front()*list.Element {
+
+//Front provides getting element from front
+func (l *List) Front() *list.Element {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
 	return l.l.Front()
 }
 
+
+//back provides getting element from back
 func (l *List) Back() *list.Element {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
