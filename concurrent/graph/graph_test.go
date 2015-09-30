8854e3461a9b28f5cb5188d906a48265316e72f8
Sergey Romanov <xxsmotur@gmail.com>
2015-09-30 21:57:57 +0500

    Add test for directed edge

--- concurrent/graph/graph_test.go
+++ concurrent/graph/graph_test.go
@@ -11,6 +11,7 @@ func constructGraph1()*Graph {
 	gr.AddNode("two")
 	gr.AddNode("bang")
 	gr.AddEdge("first", "two")
+	gr.AddDirectedEdge("first", "bang")
 	return gr
 }
 
@@ -37,5 +38,18 @@ func TestHasEdge(t *testing.T) {
 
 	if !gr.HasEdge(nodename2, nodename1) {
 		t.Errorf("Edge between %s and %s is not found", nodename2, nodename1)
+	}
+}
+
+func TestHasDirectedEdge(t *testing.T) {
+	gr := constructGraph1()
+	nodename1 := "first"
+	nodename2 := "bang"
+	if !gr.HasEdge(nodename1, nodename2) {
+		t.Errorf("Edge between %s and %s is not found", nodename1, nodename2)
+	}
+
+	if gr.HasEdge(nodename2, nodename1) {
+		t.Errorf("Edge between %s and %s is found", nodename2, nodename1)
 	}
 }
