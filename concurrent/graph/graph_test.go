db452b1530b1e2c37b0907f842474f25e970bd25
Sergey Romanov <xxsmotur@gmail.com>
2015-09-30 20:02:53 +0500

    Add tests

---
+++ concurrent/graph/graph_test.go
@@ -0,0 +1,41 @@
+package graph
+
+import
+(
+	"testing"
+)
+
+func constructGraph1()*Graph {
+	gr := NewGraph()
+	gr.AddNode("first")
+	gr.AddNode("two")
+	gr.AddNode("bang")
+	gr.AddEdge("first", "two")
+	return gr
+}
+
+func TestHasNode(t *testing.T) {
+	gr := constructGraph1()
+	nodename1 := "first"
+	nodename2 := "two"
+	if !gr.HasNode(nodename1) {
+		t.Errorf("node %s is not found", nodename1)
+	}
+
+	if !gr.HasNode(nodename2) {
+		t.Errorf("node %s is not found", nodename2)
+	}
+}
+
+func TestHasEdge(t *testing.T) {
+	gr := constructGraph1()
+	nodename1 := "first"
+	nodename2 := "two"
+	if !gr.HasEdge(nodename1, nodename2) {
+		t.Errorf("Edge between %s and %s is not found", nodename1, nodename2)
+	}
+
+	if !gr.HasEdge(nodename2, nodename1) {
+		t.Errorf("Edge between %s and %s is not found", nodename2, nodename1)
+	}
+}
