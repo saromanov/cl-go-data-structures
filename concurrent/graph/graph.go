677f4122348f58c20affafcce8f65cb6b88600d7
Sergey Romanov <xxsmotur@gmail.com>
2015-09-30 21:54:11 +0500

    Graph: Add directed edge

--- concurrent/graph/graph.go
+++ concurrent/graph/graph.go
@@ -58,6 +58,23 @@ func (node *Graph) AddEdge(innode, outnode string) error {
 	return nil
 }
 
+//AddDirectedEdge provides append only directed edge A -> B between two nodes
+func (node *Graph) AddDirectedEdge(innode, outnode string) error {
+	node1, ok := node.nodes[innode]
+	if !ok {
+		return fmt.Errorf("Node %s is not found", innode)
+	}
+
+	node2, ok2 := node.nodes[outnode]
+	if !ok2 {
+		return fmt.Errorf("Node %s is not found", outnode)
+	}
+
+	node.nodes[innode].AddOutnode(node2)
+	node.nodes[outnode].AddInnode(node1)
+	return nil
+}
+
 //HasEdge return true if innode and outnode is connected
 func (node *Graph) HasEdge(innode, outnode string) bool {
 	node1, ok := node.nodes[innode]
